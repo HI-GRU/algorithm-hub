e2d301ee7d263c3aa74d5d9a559791734aae619f
parseler <162273119+parseler@users.noreply.github.com>
2024-07-27 12:39:35 +0900

    [Gold IV] Title: 공유기 설치, Time: 316 ms, Memory: 42140 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static int n;
+    static int c;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         
+         n = Integer.parseInt(st.nextToken());
+         c = Integer.parseInt(st.nextToken());
+         
+         arr = new int[n];
+         for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
+         Arrays.sort(arr);
+
+         
+         int start = 0;
+         int end = arr[n - 1] - arr[0];
+         int ans = 0;
+         
+         while (start <= end) {
+        	 int mid = (start + end) / 2;
+        	 
+        	 if (prob(mid)) {
+        		 ans = mid;
+        		 start = mid + 1;
+        	 } else {
+        		 end = mid - 1;
+        	 }
+         }
+         
+         System.out.println(ans);
+    }
+
+	private static boolean prob(int mid) {
+
+		int cnt = n - c;
+		int[] copy = Arrays.copyOf(arr, arr.length);
+		
+		for (int i = 0; i < n - 1; i++) {
+			if (copy[i + 1] - copy[i] < mid) {
+				if (cnt == 0) return false;
+				
+				cnt--;
+				copy[i + 1] = copy[i];
+			}
+		}
+		
+		return true;
+	}
+}
