add6064080abcd2d8a2c9edb7cf40b60e61e2e27
NamSeok <162273119+parseler@users.noreply.github.com>
2024-09-24 22:25:58 +0900

    [Gold V] Title: 알약, Time: 64 ms, Memory: 11676 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        long[] dp = new long[31];
+        
+        dp[0] = 1;
+        
+        for (int i = 1; i <= 30; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+        	int n = Integer.parseInt(br.readLine());
+        	if (n == 0) break;
+        	
+        	sb.append(dp[n]).append('\n');
+        }
+        
+        System.out.println(sb);
+    }
+}
